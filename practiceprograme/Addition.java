6b6ffff9eee504fec152c2949ebe1c42675c4a1d
ABHILASHKOLTE <abhilash.kolte@gmail.com>
2021-12-31 17:17:15 +0530

    addtionOfNUmber

---
+++ practiceprograme/Addition.java
@@ -0,0 +1,14 @@
+package practiceprograme;
+
+public class Addition {
+    public static void main(String[] args) {
+
+
+        int num1 = 1;
+        int num2 = 23;
+        int num3 = 93;
+
+        int sum = num1 + num2 + num3;
+        System.out.println(sum);
+    }
+}
